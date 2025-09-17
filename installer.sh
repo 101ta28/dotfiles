97f61305fc152dee73888f4320acd0de576cb6ef
Tatsuya Imai <34620490+101ta28@users.noreply.github.com>
2025-09-17 16:43:49 +0900

    Add zsh to the list of installed packages

--- installer.sh
+++ installer.sh
@@ -198,7 +198,7 @@ done
 # =============================================================================
 log_info "Installing system packages..."
 sudo apt update
-sudo apt install -y unzip ca-certificates jq ripgrep fzf neovim kleopatra
+sudo apt install -y unzip ca-certificates jq ripgrep fzf neovim kleopatra zsh
 
 # =============================================================================
 # 開発ツールのインストール
